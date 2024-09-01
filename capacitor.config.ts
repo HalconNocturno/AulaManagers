9c50364af5f2aa61ee1e3b94c0b114f2d5e8f4a2
HalconNocturno <167113946+HalconNocturno@users.noreply.github.com>
2024-09-01 13:00:45 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aula_Manager',
+  webDir: 'www'
+};
+
+export default config;
